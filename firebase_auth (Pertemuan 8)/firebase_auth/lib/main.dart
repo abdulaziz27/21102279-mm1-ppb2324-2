6f7874d0466f36d6769061de94fd768bd83a9779
Abdul Aziz <doelziez212@gmail.com>
2024-05-10 16:16:24 +0700

    Modul Praktikum 10

--- firebase_auth (Pertemuan 8)/firebase_auth/lib/main.dart
+++ firebase_auth (Pertemuan 8)/firebase_auth/lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:firebaseAuth/ui/home_screen.dart';
+import 'package:firebaseAuth/ui/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -30,7 +33,22 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         navigatorKey: NAV_KEY,
         onGenerateRoute: generateRoute,
-        home: SplashScreen(),
+        home: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasData) {
+              return HomeScreen();
+            } else if (snapshot.hasError) {
+              return const Center(
+                child: Text('Something went wrong'),
+              );
+            } else {
+              return const LoginScreen();
+            }
+          },
+        ),
       ),
     );
   }
